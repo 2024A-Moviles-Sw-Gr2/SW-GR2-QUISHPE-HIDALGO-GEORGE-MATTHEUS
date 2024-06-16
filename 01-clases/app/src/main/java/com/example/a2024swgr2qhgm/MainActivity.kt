6e1a28df2e8d36d4ffe0515742f7e90276abe764
George Quishpe <george.quishpe@epn.edu.ec>
2024-06-16 18:10:49 -0500

    feat: trabajo en clase con qualifiers

--- 01-clases/app/src/main/java/com/example/a2024swgr2qhgm/MainActivity.kt
+++ 01-clases/app/src/main/java/com/example/a2024swgr2qhgm/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a2024swgr2qhgm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
