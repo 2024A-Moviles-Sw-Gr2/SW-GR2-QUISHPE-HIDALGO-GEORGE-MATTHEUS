fab6a7b8b80dbe22673362399640453ec78ebdf2
George Quishpe <george.quishpe@epn.edu.ec>
2024-06-16 18:20:15 -0500

    feat: ciclo de vida de una actividad

--- 01-clases/app/src/main/java/com/example/a2024swgr2qhgm/ACicloVida.kt
+++ 01-clases/app/src/main/java/com/example/a2024swgr2qhgm/ACicloVida.kt
@@ -5,16 +5,49 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class ACicloVida : AppCompatActivity() {
+
+    var textoGlobal = ""
+    fun mostrarSnackbar(texto:String){
+        textoGlobal += texto
+        val snack = Snackbar.make(
+            findViewById(R.id.cl_ciclo_vida),
+            textoGlobal,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snack.show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_aciclo_vida)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        mostrarSnackbar("OnCreate")
     }
+    override fun onStart(){
+        super.onStart()
+        mostrarSnackbar("OnStart")
+    }
+    override fun onResume(){
+        super.onResume()
+        mostrarSnackbar("onResume")
+    }
+    override fun onRestart(){
+        super.onRestart()
+        mostrarSnackbar("onRestart")
+    }
+    override fun onPause(){
+        super.onPause()
+        mostrarSnackbar("onPause")
+    }
+    override fun onStop(){
+        super.onStop()
+        mostrarSnackbar("onStop")
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        mostrarSnackbar("onDestroy")
+    }
+
 }
--- 01-clases/app/src/main/java/com/example/a2024swgr2qhgm/MainActivity.kt
+++ 01-clases/app/src/main/java/com/example/a2024swgr2qhgm/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.a2024swgr2qhgm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botonCicloVida = findViewById<Button>(
+            R.id.btn_ciclo_vida
+        )
+        botonCicloVida
+            .setOnClickListener {
+                irActividad(ACicloVida::class.java)
+            }
     }
 
     fun irActividad(
