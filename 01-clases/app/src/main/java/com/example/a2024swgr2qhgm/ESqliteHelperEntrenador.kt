6210d7202e751f757932538ba84f648400ae2440
George Quishpe <george.quishpe@epn.edu.ec>
2024-07-11 00:25:59 -0500

    feat: introducción sqlite

---
+++ 01-clases/app/src/main/java/com/example/a2024swgr2qhgm/ESqliteHelperEntrenador.kt
@@ -0,0 +1,48 @@
+package com.example.a2024swgr2qhgm
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class ESqliteHelperEntrenador (
+    contexto: Context? // this
+): SQLiteOpenHelper(
+    contexto,
+    "moviles",
+    null,
+    1
+){
+    override fun onCreate(db: SQLiteDatabase?) {
+        val scriptSQLCrearTablaEntrenador =
+            """
+                CREATE TABLE ENTRENADOR(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nombre VARCHAR(50),
+                    descripcion VARCHAR(50)
+                )
+            """.trimIndent()
+        db?.execSQL(scriptSQLCrearTablaEntrenador)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun crearEntrenador(
+        nombre:String,
+        descripcion:String
+    ):Boolean{
+        val basedatosEscritura = writableDatabase
+        val valoresAGuardar = ContentValues()
+        valoresAGuardar.put("nombre", nombre)
+        valoresAGuardar.put("descripcion", descripcion)
+        val resultadoGuardar = basedatosEscritura.insert(
+            "ENTRENADOR",
+            null,
+            valoresAGuardar
+        )
+        basedatosEscritura.close()
+        return if(resultadoGuardar.toInt()==-1) false else true
+    }
+}
---
+++ 01-clases/app/src/main/java/com/example/a2024swgr2qhgm/EbaseDeDatos.kt
@@ -0,0 +1,7 @@
+package com.example.a2024swgr2qhgm
+
+class EbaseDeDatos {
+    companion object {
+        var tablaEntrenador: ESqliteHelperEntrenador? = null
+    }
+}
